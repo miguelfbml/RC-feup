31e49ad99caa7b4ec4e7e4bd8333d1fc690745e6
miguelfbml <up202108659@edu.fe.up.pt>
2023-10-10 22:44:55 +0100

    mais

--- Proj/highlevel.c
+++ Proj/highlevel.c
@@ -1,5 +1,61 @@
-SENTA de LADIN no pau
-o lousado é muita feio
 
 
 
+
+
+
+
+
+
+
+
+enum Status {RECEIVER = 0, TRANSMITTER = 1 }; 
+
+
+
+
+int llopen(int porta, enum Status status)  {
+    switch (status)
+    {
+    case RECEIVER:
+        /* code */
+
+
+
+
+
+
+
+
+
+        break;
+
+    case TRANSMITTER:
+        /* code */
+
+
+
+
+
+
+
+
+
+        break;
+
+    default:
+        break;
+    }
+
+
+
+}
+
+
+
+argumentos –porta: COM1, COM2, ...  –flag: TRANSMITTER / RECEIVER 
+retorno –identificador da ligação de dados
+
+
+
+–valor negativo em caso de erro
---
+++ Proj/teste.c
@@ -0,0 +1,67 @@
+// Write to serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <signal.h>
+#include <stdio.h>
+
+#define FALSE 0
+#define TRUE 1
+
+
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+#define BUF_SIZE 5 //256
+
+
+volatile int STOP = FALSE;
+
+
+int alarmEnabled = FALSE;
+int alarmCount = 0;
+
+// Alarm function handler
+void alarmHandler(int signal)
+{
+    alarmEnabled = FALSE;
+    alarmCount++;
+
+    printf("Alarm #%d\n", alarmCount);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+
+    (void)signal(SIGALRM, alarmHandler);
+    // Program usage: Uses either COM1 or COM2
+    
+
+
+
+    While(alarmCount < 4){
+
+
+    }
+
+
+
+    return 0;
+}
